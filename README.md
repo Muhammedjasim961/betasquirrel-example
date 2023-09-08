8aa658f71256b562be52b50e335aa80bc5f76205
Muhammedjasim961 <jasimwayanad961@gmail.com>
2023-09-07 12:23:28 -1200

    javascrit completed

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # betasquirrel-example
 task from betasquirrel
+
+# suzuki
+Assignment from suzuki
+
+## Requirements
+
+|    Requirements               | Output |
+| ------------------------------| ------ |
+| ![Task 1](images/task-1.png)  | two    |
+| ![Task 2](images/task-1.png)  | two    |
+
+### formating
+
+**lorejnfjfsckcnn**cece;knvnvmnc4y*roiwue*cand;u4h2j ~~d3hrbfhe~~
+
+`git commit`
+
+
+## Linux Camands
+
+- `cd` - change current directory. Ex: `cd desktop`, `cd ..`
+- `is` - List contents pf a directory, Ex: `ls -a` lists hidden files as well.
+- `pwd` - Display current working directory path
+- `Cat` - Display contents of file. Ex: `cat README.md` 
+
+## Git Commands
+
+1. `git config` configure git user. Ex: `git config --global user.name "Muhammedjasim961"`, `git config user.email "jasimwayanad961@gmail.com"`
+2. `git clone` clone a remote git repo to your local. Ex: `git clone https://github.com/Muhammedjasim961/suzuki.git`, `git clone https://github.com/Muhammedjasim961/suzuki.git new-folder`
+3. `git add` Add your file changes to git. Ex: `git add . ` `gitadd README.md`
+4. `git commit` commit changes to git. Ex: `git commit -m "initial commit"`
+5. ` git push` push your local commits to remote repo. Ex: `git push origin main`
+
+## task 3
+
+read the below pages and attend the tests
+
+- [HTML](htttps://www.w3schools.com/html/defalt.asp)
+- [Attributes](https://www.w3schools.com/html/html_attributes.asp)
+
+
+# Responsive Design Dimensions
+
+- xs ( >=576 )
+- sm ( >=576 )
+- md (>=768 )
+- lg (>= 992 )
+- xl (>=1200 )
+-xxl (>=1400 )
+
+
+- mobile `@media only screen and (max-width: 767px) {}`
+- tablet `@media only screen and (max-width: 991px) and (min-width: 768) {}`
--- javascript/js/app.js
+++ javascript/js/app.js
@@ -1,211 +1,63 @@
-
 //alert("hello")
+//console.log("hello world");
 
-// console.log("one")
-// console.error("two")
-// console.debug("three")
-// console.warn("four")
-// console.clear()
+// let num1 = 128;
+// let num2 = 12;
+// let result;
+// console.log(num1 + num2);
 
-// var firstName = "muhammed"; //string
-// var lastName = "jasim"; //string
-// const isOnLeave = true; // boolean
-// let distanceFromOffice = 6.1; // number
+// function findSum( num1, num2) {
 
-
-// firstName = "Aslam"; //reassign
-// distanceFromOffice = 7;
-
-
-
-// console.log(firstName + " " + lastName)
-
-
-// let num1 = 4;
-// console.log(typeof numb1)
-
-// num1 = "5"
-// console.log(typeof numb1)
-
-// let num1 = 10;
-// console.log(typeof num1)
-
-// num1 = "7"; 
-// console.log(typeof num1)
-
-// let num1 = 6;
-// let num2 = 2;
-
-//  let sum = num1 * num2;
-//  alert(sum)
-
-
-// function  findsum(num1, num2) { //parameter
-// //function body
-
-// let sum = num1 + num2;
-// alert(sum);
+//    let sum = num1 + num2;
+//    alert(sum)
 // }
-
-//   //argument
-//   findsum(3, 2);
-//   findsum(2, 4);
-//   findsum(1, 2);
-
-
-// function findsum(num1, num2) {
-
-//     return num1 + num2;
-    
-// }
-// let sum = findsum(7,9);
-// alert(sum);
-// console.log(sum);
-
-// let result = sum + 10;
-// alert(result)
-// console.log(result)
+// findSum(10,33);
+// findSum(10, 44);
+// findSum(11, 22)
 
 //condition base
 
-// let age = 17;
-// //  let result = age >= 18;
-// //  alert(result)
-// // console.log(result)
+// let age = 19;
 
-// if (age >=18) {
-//     alert('Your application submitted');
+// if(age >= 18) {
+//     alert("you are eligible for the exam");
 // }else {
-//     alert('You are not eligible at this age');
+//     alert("you are not eligible for the application at this age");
 // }
 
-// let color = "brown";
-//Dom Document object model   //DOM manipulation
-// function findFavoriteFruit() {
+// let color = "orange";
 
-//     const color = document.getElementById("color").value;
 
-//     if (color === "orange") { 
-//        console.log("You like to eat orange!");
-//      }else if (color === "yellow") {
-//        console.log("You like to eat banana!");
-//     }else { 
-//        console.log ("You don't like fruits!");
+
+//DOM DOCUMENT OBJECT MODEL - DOM MANIPULATION;
+
+//let li = document.querySelector("li")
+//console.log(li);
+
+//let element = document.getElementById("color");
+
+// function findFruits() {
+//     let element = document.getElementById("color")
+
+//     console.log(element.style.backgroundColor)
+// }
+
+//OBJECT ORIENTED PROGRAMME
+// let student = { firstName:"jasim",
+//     lastName:"hussain", 
+//     age:22,
+//     isPassed:true }      
+
+//     let car = {
+//         model: "volvo",
+//         drive: function () {
+//             console.log('bhuoom')
+//         }
 //     }
-// // }
+//     console.log(car.drive());
 
-// const element = document.getElementById("color");
-// console.log(element)
+//ARREYS
 
+let fruits =  ["mango", "pineapple", "watermaloon", 8.8, false];
 
-//const element = document.getElementById("color")
-// //console.log(element)
-// const color = document.getElementById("color").value;
- //console.log(color)
-
-// if (color === "orange") { 
-//     console.log("You like to eat orange!");
-//   }else if (color === "yellow") {
-//     console.log("You like to eat banana!");
-//  }else { 
-//     console.log("You don't like fruits");
-//  }
-               
-
-//      function findFavoriteFruit () {
-
-// const color = document.getElementById("color").value;
-
-// if (color === "orange") { 
-//     console.log("You like to eat orange!");
-// }else if (color === "yellow") {
-//     console.log("You like to eat banana!");
-// }else { 
-//     console.log("You don't like fruits");
-//  }  
-//      }
-
-//getter,setter
-
-
-
-// String ''," "
-// Number 5.5,5, -3
-// Boolean
-// undefined[ typeof !]
-// null
-// BigInt [big numbers 1234567890]64bit
-// Symbol
-// Object - Object,array,date
-// function
-
-//let a = null;
-//console.log(typeof a)
-// function abc (){
-// return 4;
-// }
-
-// let result = abc();
-// console.log(typeof result);
-
-
-//object - student , staff , exam , fee
-
-// let student = { 
-//   firstName: "jasim",
-//   lastName: "hussein",
-//   age:22,
-//   isPassed: true
-//  };
-
-//  const car = {
-//      model: "volvo xc60",
-//      drive:function () {
-//         console.log("bhooom");
-//      },
-//  };
-//  console.log(car.drive(4))
-
-// let student = 
-// { firstName: "aslam",
-//   lastName: "jihad",
-//   age:22,
-//   isPassed: true
-// };
-// //console.log(firstName)
-
-// const car = {
-//      model: "volvo xc60",
-//      drive:function() {
-//       console.log("thaat")
-//      },
-// };
-
-// console.log(car.drive())
-
-//object oriented programme
-
-// let student = { firstName: "ansir", lastName: "rinas", age: 22, qualification:"masscom", house: "pinangode",}
-// const bike = { model:"interceptor", color: "red", number: 1234, drive: function() { console.log('tata')
-// return  ;
-// },
-// }
-// console.log(bike.drive())
-
-//json
-//xml extended markup language
-
-
-//  const student = {firstName: "mubashir"}
-//  student.lastName = "ahammed"
-//  console.log(student)
-
-//ARRAYS/
-
-//const fruits = new Array ("apple, orange, banana, 7, false")
-//const fruits = ["apple", "orange", "banana", 7, false];
-
-//console.log(fruits[fruits.length - 1]);
-
-const books = ["malayalam", "english", "hindi", "urdu", "arabic"];
-console.log(books[books.length - 1])
+console.log( fruits[fruits.length - 2]);
